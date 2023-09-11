510b3a7130e1e336de3f66d4b358d99f9d7425c5
gabigringavolei <130366455+gabigringavolei@users.noreply.github.com>
2023-09-11 08:46:37 -0300

    primeiro

---
+++ introducao-javascript/js/principal.js
@@ -0,0 +1,13 @@
+//console.log("o arquivo separado esta funcionando");
+
+//---------------Daqui para baixo começa o codigo
+ 
+//---------------alteraçao do titulo
+//-----console.log(document);
+//-----cria a vareavel titulo e almazena o conteudo da clase 
+let titulo = document.querySelector(".titulo");
+//-----console.log(titulo);
+//-----atrivuiçao de um novo valor na variavel titulo
+titulo.textContent = "fake natty nutriçao!";
+
+//---------------alteraçao da tabela 
